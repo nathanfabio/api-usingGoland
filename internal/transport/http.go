12256409c66b33593bd64d6e50da99c08258f969
Nathan Fabio <nathanfabio.dev@gmail.com>
2024-11-23 22:39:36 -0300

    feat: add http.go

---
+++ internal/transport/http.go
@@ -0,0 +1,52 @@
+package transport
+
+import (
+	"encoding/json"
+	"github.com/nathanfabio/api-usingGoland/internal/todo"
+	"log"
+	"net/http"
+)
+
+type TodoItem struct {
+	Item string `json:"item"`
+}
+
+type Server struct {
+	mux *http.ServeMux
+}
+
+func NewServer(todoSvc *todo.Service) *Server {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /todo", func(w http.ResponseWriter, r *http.Request) {
+		b, err := json.Marshal(todoSvc)
+		if err != nil {
+			log.Println(err)
+		}
+
+		_, err = w.Write(b)
+		if err != nil {
+			log.Println(err)
+		}
+	})
+
+	mux.HandleFunc("POST /todo", func(writer http.ResponseWriter, request *http.Request) {
+		var t TodoItem
+		err := json.NewDecoder(request.Body).Decode(&t)
+		if err != nil {
+			log.Println(err)
+			writer.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		todoSvc.Add(t.Item)
+		writer.WriteHeader(http.StatusCreated)
+		return
+	})
+
+	return &Server{mux: mux}
+}
+
+func (s *Server) Run() error {
+	return http.ListenAndServe(":8080", s.mux)
+}
--- main.go
+++ main.go
@@ -1,53 +1,18 @@
 package main
 
 import (
-	"encoding/json"
 	"github.com/nathanfabio/api-usingGoland/internal/todo"
+	"github.com/nathanfabio/api-usingGoland/internal/transport"
 	"log"
-	"net/http"
 )
-
-type TodoItem struct {
-	Item string `json:"item"`
-}
 
 func main() {
 
 	svc := todo.NewService()
+	server := transport.NewServer(svc)
 
-	mux := http.NewServeMux()
-
-	mux.HandleFunc("GET /todo", func(w http.ResponseWriter, r *http.Request) {
-		b, err := json.Marshal(svc)
-		if err != nil {
-			log.Println(err)
-		}
-
-		_, err = w.Write(b)
-		if err != nil {
-			log.Println(err)
-		}
-	})
-
-	mux.HandleFunc("POST /todo", func(writer http.ResponseWriter, request *http.Request) {
-		var t TodoItem
-		err := json.NewDecoder(request.Body).Decode(&t)
-		if err != nil {
-			log.Println(err)
-			writer.WriteHeader(http.StatusBadRequest)
-			return
-		}
-
-		svc.Add(t.Item)
-		writer.WriteHeader(http.StatusCreated)
-		return
-	})
-
-	mux.HandleFunc("DELETE /", func(w http.ResponseWriter, r *http.Request) {
-
-	})
-
-	if err := http.ListenAndServe(":8080", mux); err != nil {
+	if err := server.Run(); err != nil {
 		log.Fatal(err)
 	}
+
 }
